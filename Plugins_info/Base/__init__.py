108bd8969fa360d0bd0c11a3c779192aadb9eddc
QaQNotFound <cg18626426526@163.com>
2022-07-25 21:17:42 +0800

    db

---
+++ Plugins_info/Base/__init__.py
@@ -0,0 +1,13 @@
+import sqlite3
+from pathlib import Path
+import time
+db = Path()
+
+async def get_public_cookie():
+    connect = sqlite3.connect(db)
+    cursor = connect.cursor()
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS public_cookie'''
+
+
+    )
